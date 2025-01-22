62c507e9b05ee8d0f2aaf48cb55c7b4fa582467d
adha21 <adhamastalulala@gmail.com>
2025-01-22 10:46:04 +0700

    memperbarui link beranda

--- navbar.php
+++ navbar.php
@@ -7,7 +7,7 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="/is62/PROJECT/index.php">Beranda</a>
+          <a class="nav-link active" aria-current="page" href="/uas_web_adha/uas_web_adha/index.php">Beranda</a>
         </li>
         <li class="nav-item">
           <a class="nav-link active" aria-current="page" href=#>Buku</a>
