6f5f85571c8133e05ce55880c15cb7866a740bc4
adha21 <adhamastalulala@gmail.com>
2025-01-22 14:48:48 +0700

    menambahkan tombol detail

--- modul-buku/index.php
+++ modul-buku/index.php
@@ -60,6 +60,29 @@
                             <i class="fa-solid fa-trash"></i>
                             </button>
 
+                            <!-- TOMBOL DETAIL -->
+                            <button type="button" class="btn btn-warning btn-sm" data-bs-toggle="modal" data-bs-target="#detail<?=$data['id']?>">
+                            <i class="fa-solid fa-eye"></i>
+                            </button>
+
+                            <!-- MODAL DETAIL-->
+                            <div class="modal fade" id="detail<?=$data['id']?>" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal-dialog">
+                                <div class="modal-content">
+                                <div class="modal-header">
+                                    <h1 class="modal-title fs-5" id="exampleModalLabel">Detail Cover <?=$data['judul_buku']?> </h1>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div>
+                                <div class="modal-body">
+                                    <img width="200" src="foto/<?=$data['cover_buku']?>" alt="">
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Tutup</button>
+                                    
+                                </div>
+                                </div>
+                            </div>
+                            </div>
                             <!-- Modal -->
                             <div class="modal fade" id="hapus<?=$data['id']?>" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
                             <div class="modal-dialog">
